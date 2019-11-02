9d63d3456d45fa39181163473ab12a8b2e309080
cerebrium <nicholas.m.shankland@gmail.com>
2019-11-02 11:37:42 -0700

    starting on styling

--- .history/src/App_20191102113421.jsx
+++ .history/src/App_20191102113421.jsx
@@ -16,7 +16,7 @@ class App extends React.Component {
   render () {
     return (
       <Router>
-        <div className='appContainer'>
+        <div className='container'>
           <nav><Link to='/' >Home</Link> | <Link to='/about' >About</Link> | <Link to='/recruitment' >Recruitment</Link> | <Link to='/testimonials' >Testimonials</Link> | <Link to='/policies' >Policies</Link> | <Link to='/contact'>Contact</Link></nav>
           <Route exact path='/' component={Home}/>
           <Route exact path='/about' component={About}/>
--- .history/src/Routes/Home_20191102111540.jsx
+++ .history/src/Routes/Home_20191102111540.jsx
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import '../styles/Home.css'
 
 class Home extends React.Component {
     render () {
